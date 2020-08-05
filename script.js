e6e5686317907a64be6dd652f1829ffa0ca818b4
Fraser Carter <fraserc97@hotmail.co.uk>
2020-08-05 21:24:28 +0100

    added event listeners for file upload

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-let ingestFile = function() {
-
-    const reader = new FileReader();
+const ingestFile = function() {
+    const reader = new FileReader();  
     const file = document.getElementById("uploadInput").files[0];
+    
+    addListeners(reader);
 
     if (!file.name.includes(".csv")) {
         alert("please upload a csv file");
     } else {
         reader.onload = function() {
-            document.getElementById("output").innerHTML = reader.result;
+            let temp = reader.result.split("\n");
+            let csv = [];
+
+            for (let line of temp) {
+                csv.push(line.split(","));
+            }
         }
         reader.readAsText(file);
     }
+}
+
+const addListeners = function(fr) {
+    fr.addEventListener('loadstart', function(event) { console.log(event);});
+    fr.addEventListener('load', function(event) { console.log(event);});
+    fr.addEventListener('loadend', function(event) { console.log(event);});
+    fr.addEventListener('progress', function(event) { console.log(event);});
+    fr.addEventListener('error', function(event) { console.log(event);});
+    fr.addEventListener('abort', function(event) { console.log(event);});
 }
