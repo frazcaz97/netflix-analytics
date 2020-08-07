4f72a2fe0e23beae177854e7485f96bf44d4ad7c
Fraser Carter <fraserc97@hotmail.co.uk>
2020-08-07 19:47:02 +0100

    cleaned up script

--- script.js
+++ script.js
@@ -1,75 +1,26 @@
-let apikey = document.getElementById("apiKeyField").value;
+let apiKey = "";
+let csvFile = [];
+const reader = new FileReader();
 
-const ingestFile = function() {     //get file, add listeners, create csv variable
-    const reader = new FileReader();
-    const file = document.getElementById("uploadInput").files[0];
+const getFile = function() {
     
-    addListeners(reader);
+    const file = document.getElementById("uploadInput").files[0];
 
-    //if (!file.name.includes(".csv")) {
-    //    alert("please upload a csv file");
-    //} else {
-        reader.onload = function() {
-            let temp = reader.result.split("\n");
-            let csv = [];
+    if (!file.name.includes(".csv")) {
+        alert("please upload a csv file");
+    } else {
+        reader.onprogress = function(event) {
+            console.log("current: " + event.loaded);
+            console.log("total: " + event.total);
+        }
+        reader.onload = function(event) {
+            let newCSV = new csv(event.target.result);
+            newCSV.create();
+            csvFile = newCSV.csv;
+            
 
-            for (let line of temp) {
-                csv.push(line.split(","));
-            }
-            getApiResults(csv);
         }
         reader.readAsText(file);
-    //}
-}
-
-const addListeners = function(fr) {
-    fr.addEventListener('loadstart', event => handleEvent(event));
-    fr.addEventListener('load', event => handleEvent(event));
-    fr.addEventListener('loadend', event => handleEvent(event));
-    fr.addEventListener('progress', event => handleEvent(event));
-    fr.addEventListener('error', event => handleEvent(event));
-    fr.addEventListener('abort', event => handleEvent(event));
-}
-
-const handleEvent = function(event) {   //unfinished file
-    console.log(event);
-
-    switch (event.type) {
-        case 'loadstart':
-            console.log("file upload started");
-            break;
-        case 'load':
-            console.log("file upload load successfully");
-            break;
-        case 'loadend':
-            console.log("file upload finished");
-            break;
-        case 'progress':
-            console.log("file upload in progress");
-            progressHistogram(event.loaded,event.total);    //tempory function
-            break;
-        case 'error':
-            console.log("file upload error");
-            alert("There was an error uploading your file please try again");
-            break;
-        case 'abort':
-            console.log("file upload aborted");
-            break;
     }
 }
 
-const progressHistogram = function(current,total) {     //unfinished function
-    console.log(`Current: ${current}`);
-    console.log(`TotalL ${total}`);
-}
-
-const getApiResults = function(csvFile) {   //unfinished function
-    console.log(`csv: ${csvFile}`);
-}
-
-const apiRequest = function(title) {
-    const request = fetch(`http://www.omdbapi.com/?t=${title}&apikey=${apikey}`)
-        .then(response => response.json())
-        .then(data => data);
-    return request;
-}
