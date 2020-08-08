398d5049e0facff98a308935463f9a3db833dbf3
Fraser Carter <fraserc97@hotmail.co.uk>
2020-08-08 14:58:35 +0100

    request function now returns the data itself

--- apiRequest.js
+++ apiRequest.js
@@ -2,17 +2,12 @@ class apiRequest {
     constructor(title,apiKey) {
         this._title = title;
         this._apiKey = apiKey;
-        this._data = null;
     }
 
-    async runRequest() {
+    async readRequest() {
         const url = encodeURI(`http://www.omdbapi.com/?t=${this._title}&apikey=${this._apiKey}`)
         const request = await fetch(url)
-        const data = await request.json();
-        return data;
-    }
-
-    get data() {
-        return this._data;
+        const obj = await request.json();
+        return await obj;
     }
 }
