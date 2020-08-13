d26dabc081c7a22401c149b60627175dc64ad602
Fraser Carter <fraserc97@hotmail.co.uk>
2020-08-13 22:20:40 +0100

    refactored code, added comments

--- js/csv.js
+++ js/csv.js
@@ -5,16 +5,20 @@ class csv {
     }
 
     create() {
-        let arr = this._file.split("\n");
+        let arr = this._file.split("\n");   //each new line is a row
+        
+        arr.shift();    //we don't need the headers
+        arr.pop();  //remove empty index from split
 
         for (let index of arr) {
-            let subarr = index.split('","');
+            let subarr = index.split('","');    //split up the row by title and data
 
             for (let index in subarr) {
-                subarr[index] = subarr[index].replace('"', '');
+                subarr[index] = subarr[index].replace('"', ''); //remove additional speach marks created by text upload
             }
             this._csv.push(subarr);
         }
+        console.log(this._csv);
     }
 
     get csv() {
