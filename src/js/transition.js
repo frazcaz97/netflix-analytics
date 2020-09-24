fee33206142c46ea8f94d1c7fbd98afa0dfc788d
Fraser Carter <fraserc97@hotmail.co.uk>
2020-09-24 20:29:11 +0100

    refactored to not be a module

--- src/js/transition.js
+++ src/js/transition.js
@@ -2,7 +2,7 @@ const styleSheets = document.styleSheets[0];
 let isHomePage = true;
 let isAnalyticsPage = false;
 
-export const transition = () => {
+const transition = () => {
 
     if (isHomePage) {
         styleSheets.insertRule("#screen-animation {position: absolute; z-index: 100; top: 0%; left: 0%; right: 0%; bottom: 0%; max-width: 100%; max-height: 100%; width: 100%; height: 100%; background: #e50914; animation: wipe-left 3s ease-in-out;}",0);
